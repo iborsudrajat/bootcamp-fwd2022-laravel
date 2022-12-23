cee7422fa637660ac441f28f3565efcdb6e3b5d9
iborsudrajat <robby.sudrajat@gmail.com>
2022-12-23 09:17:55 +0700

    add foreign keys in table detail user

--- database/migrations/2022_12_19_015124_create_appointment_table.php
+++ database/migrations/2022_12_19_015124_create_appointment_table.php
@@ -15,7 +15,15 @@ return new class extends Migration
     {
         Schema::create('appointment', function (Blueprint $table) {
             $table->id();
+            $table->integer('doctor_id');
+            $table->integer('user_id');
+            $table->integer('consultation_id');
+            $table->enum('level',[1,2,3]);
+            $table->date('date')->nullable();
+            $table->time('time')->nullable();
+            $table->enum('status',[1,2]);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2022_12_20_231905_add_foreign_keys_to_role_user_table.php
+++ database/migrations/2022_12_20_231905_add_foreign_keys_to_role_user_table.php
@@ -13,11 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('detail_user', function (Blueprint $table) {
-            $table->foreign('user_id', 'fk_detail_user_to_users')
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->foreign('role_id', 'fk_role_user_to_role')
+            ->references('id')->on('role')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id', 'fk_role_user_to_users')
             ->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('type_user_id', 'fk_detail_user_to_type_user')
-            ->references('id')->on('type_user')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
@@ -28,9 +28,9 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('detail_user', function (Blueprint $table) {
-            $table->dropForeign('fk_detail_user_to_users');
-            $table->dropForeign('fk_detail_user_to_type_user');
+        Schema::table('role_user', function (Blueprint $table) {
+            $table->dropForeign('fk_role_user_to_role');
+            $table->dropForeign('fk_role_user_to_users');
         });
     }
 };
