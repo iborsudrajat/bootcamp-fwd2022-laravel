19795bd31b33f36b11b30f9b246e56cbf7d3cb77
iborsudrajat <robby.sudrajat@gmail.com>
2023-01-14 08:18:36 +0700

    fixing typo counsultation seeder

--- database/seeders/ConsultationSeeder.php
+++ database/seeders/ConsultationSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\MasterData\Consulatation;
+use App\Models\MasterData\Consultation;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
