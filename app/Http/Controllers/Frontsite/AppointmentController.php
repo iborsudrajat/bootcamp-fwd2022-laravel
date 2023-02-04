c670d393956af711ce05aabc9ad9bbaf04484453
iborsudrajat <robby.sudrajat@gmail.com>
2023-02-04 17:35:55 +0700

    test

--- app/Http/Controllers/Frontsite/AppointmentController.php
+++ app/Http/Controllers/Frontsite/AppointmentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Frontsite;
 use App\Http\Controllers\Controller;
 
 //use library here
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,6 +16,8 @@ Use Auth;
 // models here
 use App\Models\User;
 use App\Models\Operational\Doctor;
+use App\Models\Operational\Appointment;
+use App\Models\MasterData\Specialist;
 use App\Models\MasterData\Consultation;
 
 
@@ -39,7 +42,7 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        return view('pages.frontsite.appointment.index');
+        return abort(404);
     }
 
     /**
@@ -60,7 +63,19 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
-        return abort(404);
+        $data = $request->all();
+
+        $appointment = new Appointment;
+        $appointment->doctor_id = $data['doctor_id'];
+        $appointment->user_id = Auth::user()->id;
+        $appointment->consultation_id = $data['consultation_id'];
+        $appointment->level = $data['level_id'];
+        $appointment->date = $data['date'];
+        $appointment->time = $data['time'];
+        $appointment->status = 2; // set to waiting payment
+        $appointment->save();
+
+        return redirect()->route('payment.appointment', $appointment->id);
     }
 
     /**
@@ -106,5 +121,16 @@ class AppointmentController extends Controller
     public function destroy($id)
     {
         return abort(404);
+    }
+
+
+    // custom
+
+    public function appointment($id)
+    {
+        $doctor = Doctor::where('id', $id)->first();
+        $consultation = Consultation::orderBy('name', 'asc')->get();
+
+        return view('pages.frontsite.appointment.index', compact('doctor', 'consultation'));
     }
 }
