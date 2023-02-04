d5799881eadb7fac05f02b157ab8605270699bc4
iborsudrajat <robby.sudrajat@gmail.com>
2023-02-04 21:27:03 +0700

    restart controller

--- app/Http/Controllers/Frontsite/AppointmentController.php
+++ app/Http/Controllers/Frontsite/AppointmentController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Frontsite;
 use App\Http\Controllers\Controller;
 
 //use library here
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -16,8 +15,6 @@ Use Auth;
 // models here
 use App\Models\User;
 use App\Models\Operational\Doctor;
-use App\Models\Operational\Appointment;
-use App\Models\MasterData\Specialist;
 use App\Models\MasterData\Consultation;
 
 
@@ -42,7 +39,7 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        return abort(404);
+        return view('pages.frontsite.appointment.index');
     }
 
     /**
@@ -63,19 +60,7 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
-        $data = $request->all();
-
-        $appointment = new Appointment;
-        $appointment->doctor_id = $data['doctor_id'];
-        $appointment->user_id = Auth::user()->id;
-        $appointment->consultation_id = $data['consultation_id'];
-        $appointment->level = $data['level_id'];
-        $appointment->date = $data['date'];
-        $appointment->time = $data['time'];
-        $appointment->status = 2; // set to waiting payment
-        $appointment->save();
-
-        return redirect()->route('payment.appointment', $appointment->id);
+        return abort(404);
     }
 
     /**
@@ -128,9 +113,6 @@ class AppointmentController extends Controller
 
     public function appointment($id)
     {
-        $doctor = Doctor::where('id', $id)->first();
-        $consultation = Consultation::orderBy('name', 'asc')->get();
-
-        return view('pages.frontsite.appointment.index');
+        return abort(404);
     }
 }
--- app/Http/Controllers/Frontsite/LandingController.php
+++ app/Http/Controllers/Frontsite/LandingController.php
@@ -22,8 +22,15 @@ use App\Models\MasterData\Specialist;
 
 class LandingController extends Controller
 {
-    
-
+    /** 
+     * create a new controller instance
+     * 
+     * @return void
+    */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
