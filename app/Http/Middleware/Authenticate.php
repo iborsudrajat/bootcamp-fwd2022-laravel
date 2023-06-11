743486246821a023d4772c15bdbb1bd879e2bd57
iborsudrajat <robby.sudrajat@gmail.com>
2023-06-11 08:46:45 +0700

    update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -17,5 +17,8 @@ class Authenticate extends Middleware
         if (! $request->expectsJson()) {
             return route('login');
         }
+
+        //redirect to homepage login
+        return route ('index');
     }
 }
