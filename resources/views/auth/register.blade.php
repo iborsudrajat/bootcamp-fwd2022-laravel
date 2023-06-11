e89437587651dfbde6312f97a4dac46dc2a0ed7d
iborsudrajat <robby.sudrajat@gmail.com>
2023-06-11 09:44:40 +0700

    fix

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,71 +4,71 @@
 
 @section('content')
 
-<div class="min-h-screen">
+    <div class="min-h-screen">
         <div class="grid lg:grid-cols-2">
             <!-- Form -->
             <div class="px-4 lg:px-[91px] pt-10">
 
-             <!-- Logo Brand -->
-             <a
+                <!-- Logo Brand -->
+                <a
                     href="{{ route('index') }}"
                     class="flex-shrink-0 inline-flex items-center"
                 >
 
-                <img
+                    <img
                         class="h-12 lg:h-16"
                         src="{{ asset('/assets/frontsite/images/logo.png') }}"
                         alt="Meet Doctor Logo"
                     />
 
-                    </a>
+                </a>
 
-<div class="flex flex-col justify-center py-14 h-full lg:min-h-screen">
-    <h2 class="text-[#1E2B4F] text-4xl font-semibold leading-normal">
-        Improve Your <br />
-        Health With Expert
-    </h2>
-    <div class="mt-12">
+                <div class="flex flex-col justify-center py-14 h-full lg:min-h-screen">
+                    <h2 class="text-[#1E2B4F] text-4xl font-semibold leading-normal">
+                        Improve Your <br />
+                        Health With Expert
+                    </h2>
+                    <div class="mt-12">
 
-        <!-- Form input -->
-        <form method="POST" action="{{ route('register') }}" class="grid gap-6">
+                        <!-- Form input -->
+                        <form method="POST" action="{{ route('register') }}" class="grid gap-6">
 
-            {{-- token here --}}
-            @csrf
+                            {{-- token here --}}
+                            @csrf
 
-            <label class="block">
-                <input
-                    for="name" type="text" id="name" name="name"
-                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
-                    placeholder="Complete Name" value="{{ old('name') }}" required autofocus
-                />
+                            <label class="block">
+                                <input
+                                    for="name" type="text" id="name" name="name"
+                                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
+                                    placeholder="Complete Name" value="{{ old('name') }}" required autofocus
+                                />
 
-                @if ($errors->has('name'))
-                    <p class="text-red-500 mb-3 text-sm">{{ $errors->first('name') }}</p>
-                @endif
-            </label>
+                                @if ($errors->has('name'))
+                                    <p class="text-red-500 mb-3 text-sm">{{ $errors->first('name') }}</p>
+                                @endif
+                            </label>
 
-            <label class="block">
-                <input
-                    for="email" type="email" id="email" name="email"
-                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
-                    placeholder="Email Address" value="{{ old('email') }}" required autofocus
-                />
+                            <label class="block">
+                                <input
+                                    for="email" type="email" id="email" name="email"
+                                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
+                                    placeholder="Email Address" value="{{ old('email') }}" required autofocus
+                                />
 
-                @if ($errors->has('email'))
-                    <p class="text-red-500 mb-3 text-sm">{{ $errors->first('email') }}</p>
-                @endif
-            </label>
+                                @if ($errors->has('email'))
+                                    <p class="text-red-500 mb-3 text-sm">{{ $errors->first('email') }}</p>
+                                @endif
+                            </label>
 
-            <label class="block">
-                <input
-                    for="password" type="password" id="password" name="password"
-                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
-                    placeholder="Password" value="{{ old('password') }}" required autofocus
-                />
-            </label>
+                            <label class="block">
+                                <input
+                                    for="password" type="password" id="password" name="password"
+                                    class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
+                                    placeholder="Password" value="{{ old('password') }}" required autofocus
+                                />
+                            </label>
 
-            <label class="block">
+                            <label class="block">
                                 <input
                                     for="password_confirmation" type="password" id="password_confirmation" name="password_confirmation"
                                     class="block w-full rounded-full py-4 text-[#1E2B4F] font-medium placeholder:text-[#AFAEC3] placeholder:font-normal px-7 border border-[#d4d4d4] focus:outline-none focus:border-[#0D63F3]"
@@ -88,16 +88,16 @@
                                 <a href="{{ route('login') }}" class="text-center text-lg text-[#1E2B4F] font-medium bg-[#F2F6FE] px-10 py-4 rounded-full">
                                     Sign In
                                 </a>
-
-                                </form>
-
-                    </div>
+                            </div>
+                        </form>
 
                     </div>
+                </div>
+            </div>
             <!-- End Form -->
 
-             <!-- Qoute -->
-             <div class="hidden sm:block bg-[#F9FBFC]">
+            <!-- Qoute -->
+            <div class="hidden sm:block bg-[#F9FBFC]">
                 <div class="flex flex-col justify-center h-full px-24 pt-10 pb-20">
                     <div class="relative">
                         <div class="relative top-0 -left-5 mb-7">
@@ -132,8 +132,7 @@
                     </div>
                 </div>
             </div>
-
-                   <!-- End Qoute -->
+            <!-- End Qoute -->
         </div>
     </div>
 
